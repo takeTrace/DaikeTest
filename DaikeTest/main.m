f72aa58fd29b670b3a1abe608686d76478734574
Lneayce <mac@example.com>
2016-11-18 13:37:19 +0800

    Initial Commit

---
+++ DaikeTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DaikeTest
+//
+//  Created by Mac on 16/11/18.
+//  Copyright © 2016年 TakeTrace. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
